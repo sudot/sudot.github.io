972ab7234343a315c839eb6b89d916b20712c780
唐家林 <taogarfield@hotmail.com>
2020-09-22 13:42:15 +0800

    修复文件名带有转义字符导致替换失败

--- scripts/replace-images-link.js
+++ scripts/replace-images-link.js
@@ -1,6 +1,8 @@
 hexo.extend.filter.register('before_post_render', function (data) {
   var source = data.source;
   var fileName = source.substring(source.lastIndexOf('/') + 1, source.lastIndexOf('.'));
+  // 转义 ()[]. 等符号
+  fileName = fileName.replace(/([\(\)\[\]\.])/g, '\\$1')
   var regExp = RegExp(fileName + '\\/([^\\\\\\/\\:\\*\\?\\"\\<\\>\\|\\,\\)]+)', 'g');
   if (data.top_img) {
     data.top_img = data.top_img.replace(regExp, data.path + '$1');
