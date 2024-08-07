fd1c33fa946d3103b357aa3bf0a661a1c75291e3
唐家林 <taogarfield@hotmail.com>
2024-08-08 00:47:30 +0800

    修复replace-post-link

--- scripts/replace-post-link.js
+++ scripts/replace-post-link.js
@@ -1,9 +1,11 @@
 const path = require('path')
 
 hexo.extend.filter.register('before_post_render', function (data) {
+  if (!data.slug) return
   const folder = data.slug.substring(0, data.slug.lastIndexOf('/'))
   const regExp = RegExp('\\[(.*)\\]\\((.*)\\.md\\)', 'g')
   for (const group of data.content.matchAll(regExp)) {
+    if (group[2].startsWith('http')) continue
     const filePath = path
       .resolve('__posts__', folder, group[2])
       .split('__posts__')
